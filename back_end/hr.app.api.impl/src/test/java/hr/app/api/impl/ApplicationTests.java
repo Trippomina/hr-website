4793b39e42c2eed2b2fdf95c7890a9603908ef7c
Trippomina <Trippomina@gmail.com>
2023-06-01 11:49:50 +0300

    spring boot app created

---
+++ back_end/hr.app.api.impl/src/test/java/hr/app/api/impl/ApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.app.api.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
